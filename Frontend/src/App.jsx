f89ef42f3c6cef68210348b7b705bb7d75b914de
Joy Sarkar <joysarkar0538@gmail.com>
2026-04-19 17:34:08 +0530

    Initialised the Frontend directory

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={Home} />
+      <Route path="/about" element={About} />
+    </Routes>
+  )
+}
+
+export default App
---
+++ Frontend/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
